6436bb33c6d05002abe006a7217dadb3916c3550
Angel Avelar <rodri@Angels-MacBook-Air.local>
2023-08-21 18:26:30 -0700

    only use one gpu device

--- 2d_runs.sh
+++ 2d_runs.sh
@@ -8,6 +8,6 @@ do
         then
             loss="dice"
         fi
-        python main.py --data_path /Users/rodri/Documents/Caltech/SURF_2023/data/BigNeuron/tensors/ --image_shape 128 128 --spatial_dims 2 --data_type tensor --data_len 2880 --batch_size 32 --epochs 60 --run_name 2D_unet_${loss}_lr_${lr} --model UNet --dataset BigNeuron --wandb_project BigNeuron --lr $lr --channels 32 64 128 256 512 --strides 2 2 2 2 --loss $loss --out_channels $out_ch
+        CUDA_VISIBLE_DEVICES = 1 python main.py --data_path /Users/rodri/Documents/Caltech/SURF_2023/data/BigNeuron/tensors/ --image_shape 128 128 --spatial_dims 2 --data_type tensor --data_len 2880 --batch_size 32 --epochs 60 --run_name 2D_unet_${loss}_lr_${lr} --model UNet --dataset BigNeuron --wandb_project BigNeuron --lr $lr --channels 32 64 128 256 512 --strides 2 2 2 2 --loss $loss --out_channels $out_ch
     done
 done
